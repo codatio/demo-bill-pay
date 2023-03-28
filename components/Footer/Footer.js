e5950a67f033894a7dc5ed7b99a91494a9823cf1
Max Clayton Clowes <contact@mcclowes.com>
2023-03-28 13:05:46 +0100

    Fix new tab security

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,7 +6,7 @@ import { Typography } from "@codat/orchard-ui";
 export const Footer = () => {
   return (
     <div className={s.footerContainer}>
-      See more in our <a href="https://docs.codat.io/" target="_blank">docs</a>
+      See more in our <a href="https://docs.codat.io/" rel="noreferrer" target="_blank">docs</a>
     </div>
   );
 };
